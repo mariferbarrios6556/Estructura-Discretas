b6e5208e9c9e5d0df39270fea6c27107a4b33a79
María Fernanda Barrios Vega <105668179+mariferbarrios6556@users.noreply.github.com>
2026-03-26 20:38:02 -0600

    Update natural.hs

--- estructuras-discretas/practica4/natural.hs
+++ estructuras-discretas/practica4/natural.hs
@@ -2,16 +2,12 @@ module Natural where
 
 import Auxiliar
 
-data Natural = Cero | S Natural deriving (Eq, Show)
-
 -- a_natural
--- Convierte un entero a Natural
 a_natural :: Integer -> Natural
 a_natural 0 = Cero
 a_natural n = S (a_natural (n - 1))
 
 -- a_entero
--- Convierte Natural a entero
 a_entero :: Natural -> Integer
 a_entero Cero = 0
 a_entero (S n) = 1 + a_entero n
